8b2f452c44687cacf37243b798ab48752987f69a
Mark Gerow <megerow@gmail.com>
2024-03-22 11:45:13 -0700

    Ok, ready to write this up.

--- RAG/Program.cs
+++ RAG/Program.cs
@@ -8,11 +8,9 @@ using RAG.OpenAI;
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Retrieval Augmented Generation (RAG) Prototype");
 
-// Get files containing customer profiles
-string[] customerProfiles = Directory.GetFiles("C:\\Users\\meger\\source\\repos\\RAG\\RAG\\Documents\\Customers\\");
-
-string collectionName = "customer";
-bool collectionExists = false;
+//string question = "What does Minni do?";
+//string question = "Do any businesses operate in Bedrock?";
+//string question = "Who can help me get stronger?";
 
 // Writer object will save all the profiles to the vector store
 OpenAI.API openAiAPI = new OpenAI.API();
@@ -20,33 +18,45 @@ OpenAI.EmbeddingResult er = new OpenAI.EmbeddingResult();
 DataStax.API dsAPI = new DataStax.API();
 DataStax.Customers customers = new DataStax.Customers();
 
-//string question = "What does Minni do?";
-string question = "Do any businesses operate in Bedrock?";
-//string question = "Who can help me get stronger?";
+string question = "";
 
-// Get vector for the question we want to ask
-er = await openAiAPI.EmbedAsync(question);
-string json = JsonConvert.SerializeObject(er.data[0].embedding);
-Console.WriteLine("\nQuestion: "+question);
+while (true)
+{
+    // Get question from user, if blank, then exit
+    Console.Write("\nEnter a question (blank to exit): ");
+    question = Console.ReadLine();
+    if (string.IsNullOrEmpty(question)) break;
 
-FindRequest fr = new FindRequest();
-fr.find.sort.vector = er.data[0].embedding;
-FindResult fres = await dsAPI.FindAsync(fr);
+    // Get vector for the question we want to ask
+    er = await openAiAPI.EmbedAsync(question);
+    string json = JsonConvert.SerializeObject(er.data[0].embedding);
+    //Console.WriteLine("\nQuestion: " + question);
 
-Console.WriteLine($"\nResult: Company: {fres.data.documents[0].company}, Id: {fres.data.documents[0]._id}, Similarity: {fres.data.documents[0].similarity}");
+    FindRequest fr = new FindRequest();
+    fr.find.sort.vector = er.data[0].embedding;
+    FindResult fres = await dsAPI.FindAsync(fr);
+    //Console.WriteLine($"\nResult: Company: {fres.data.documents[0].company}, Id: {fres.data.documents[0]._id}, Similarity: {fres.data.documents[0].similarity}");
 
-// Get the document based on it's ID
-string profileText = File.ReadAllText("C:\\Users\\meger\\source\\repos\\RAG\\RAG\\Documents\\Customers\\" + fres.data.documents[0]._id + ".txt");
-Console.WriteLine("\nProfile:\n"+profileText);
+    // Get the document based on it's ID
+    string profileText = File.ReadAllText("C:\\Users\\meger\\source\\repos\\RAG\\RAG\\Documents\\Customers\\" + fres.data.documents[0]._id + ".txt");
+    //Console.WriteLine("\nProfile:\n" + profileText);
 
-// Get the ChatGPT response using this customer profile
-ChatRequest cr = new OpenAI.ChatRequest();
-cr.messages.Add(new ChatRequestMessage());
-cr.messages[0].content = $"Answer the following question: \"{question}\" using only the following data: \"{profileText}\"";
-string answer = openAiAPI.Chat(cr).Result.choices[0].message.content;
-Console.WriteLine("\nAnswer: " + answer);
+    // Get the ChatGPT response using this customer profile
+    ChatRequest cr = new OpenAI.ChatRequest();
+    cr.messages.Add(new ChatRequestMessage());
+    cr.messages[0].content = $"Answer the following question: \"{question}\" using only the following data: \"{profileText}\"";
+    string answer = openAiAPI.Chat(cr).Result.choices[0].message.content;
+    Console.WriteLine("\nAnswer: " + answer);
+
+}
 
 return;
+
+// Get files containing customer profiles
+string[] customerProfiles = Directory.GetFiles("C:\\Users\\meger\\source\\repos\\RAG\\RAG\\Documents\\Customers\\");
+
+string collectionName = "customer";
+bool collectionExists = false;
 
 // Loop through files
 foreach (string profile in customerProfiles)
